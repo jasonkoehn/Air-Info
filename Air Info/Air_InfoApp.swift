c81e4b2099987ece750dfb35611a478bfb17bf56
jasonkoehn <93397575+jasonkoehn@users.noreply.github.com>
2022-08-20 14:23:32 -0700

    Initial Commit

---
+++ Air Info/Air_InfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Air_InfoApp.swift
+//  Air Info
+//
+//  Created by Jason Koehn on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Air_InfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
